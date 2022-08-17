00869a6d356351436f563c6f918e39685abecc2b
Vitor Hernandez <39573883+Vitorhernandezpz@users.noreply.github.com>
2022-08-17 17:53:52 -0300

    criando func p criar usuario e verificação se usuario existe

--- comunidadeimpressionadora/models.py
+++ comunidadeimpressionadora/models.py
@@ -6,7 +6,7 @@ from flask_login import UserMixin
 def load_usuario(id_usuario):
     return Usuario.query.get(int(id_usuario))
 
-class Usuario(database.Model):
+class Usuario(database.Model, UserMixin):
     id = database.Column(database.Integer, primary_key=True)
     username = database.Column(database.String, nullable=False)
     email = database.Column(database.String, nullable=False, unique=True)
